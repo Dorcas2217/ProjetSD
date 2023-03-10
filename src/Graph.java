68eec41828de1d9b2273ed0b8ad152ac8e47e21e
20213136 <chehrazad.ouazzani@student.vinci.be>
2023-03-10 14:41:07 +0100

    feat: add read file

--- src/Graph.java
+++ src/Graph.java
@@ -1,16 +1,28 @@
 import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Graph {
 
+    public Graph(File lignes, File troncons) {
+        final Supplier<Stream<String>> supplier = () -> {
+            try {
+                return Files.lines(Paths.get(lignes.toURI()), Charset.defaultCharset());
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        };
+    }
 
-  public Graph(File lignes, File troncons) {
+    public void calculerCheminMinimisantNombreTroncons(String boileau, String alma) {
+    }
 
-  }
+    public void calculerCheminMinimisantTempsTransport(String boileau, String alma) {
 
-  public void calculerCheminMinimisantNombreTroncons(String boileau, String alma) {
-  }
-
-  public void calculerCheminMinimisantTempsTransport(String boileau, String alma) {
-
-  }
+    }
 }
