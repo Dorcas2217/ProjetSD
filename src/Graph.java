af65d8249d4375e83d6b449179a602cf13e543da
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-03-10 09:01:54 +0100

    import des fichiers et création de la classe Graph

---
+++ src/Graph.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class Graph {
+
+
+  public Graph(File lignes, File troncons) {
+
+  }
+
+  public void calculerCheminMinimisantNombreTroncons(String boileau, String alma) {
+  }
+
+  public void calculerCheminMinimisantTempsTransport(String boileau, String alma) {
+
+  }
+}
